ead9c9b76fcea909afc74aa9e76ab70ffe400241
asya perdana <asyaperdanaa@gmail.com>
2026-02-26 04:23:40 +0700

    Bust PWA cache for latest split-bill input fixes

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_VERSION = 'v2';
+const CACHE_VERSION = 'v3';
 const CACHE_NAME = `expense-tracker-${CACHE_VERSION}`;
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
-  './css/style.css',
-  './js/script.js',
+  './css/style.css?v=20260225-2',
+  './js/script.js?v=20260225-2',
   './assets/icon.svg',
   './manifest.json'
 ];
